5c4a57e318b7a574e6b13d42c17815703ebc42a0
AdnanFSWD <97546240+AdnanFSWD@users.noreply.github.com>
2022-04-10 19:56:29 +0530

    Add files via upload

---
+++ Module test-10-apr/index.js
@@ -0,0 +1,15 @@
+// let weather = {
+//     'Apikeys' = "e03fd4deac19f6b1c3693f53dca47956"
+// }
+
+// function getWeatherData() {
+//     navigator.geolocation.getCurrentPosition((success) => (
+//         console.log(success)
+//     ))
+
+// }
+
+for (var i = 0; i < 10; i++) {
+    console.log("Hello")
+}
+console.log(i)
